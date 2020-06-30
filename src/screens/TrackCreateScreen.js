a5c78013001a76f1037f6ee97218f80a8f65b310
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-30 09:12:39 -0400

    boilerplate code

---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Stylesheet, Text } from "react-native";
+
+const TrackCreateScreen = () => {
+  return (
+    <View>
+      <Text>Track Create Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackCreateScreen;
