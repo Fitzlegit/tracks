741547d4634a3ff23203d6ec0cbf3d65e51f79fc
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-30 09:05:09 -0400

    boilerplate code

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SigninScreen = () => {
+  return (
+    <View>
+      <Text>SigninScreen</Text>
+    </View>
+  );
+};
+
+const styles = Stylesheet.create({});
+
+export default AccountScreen;
