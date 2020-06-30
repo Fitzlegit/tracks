ed120860793c394bbcff439d5e22a5a7554c64dc
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-30 09:12:51 -0400

    boilerplate code

---
+++ src/screens/TrackDetailScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Stylesheet, Text } from "react-native";
+
+const TrackDetailScreen = () => {
+  return (
+    <View>
+      <Text>Track Detail Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackDetailScreen;
